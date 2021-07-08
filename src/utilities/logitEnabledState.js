c38738ab7322f5271727c85cb4dc6cf15f397ae6
Aidan Nichol <aidan@nicholware.co.uk>
2021-07-08 12:01:03 +0100

    rationalise the string display

--- src/utilities/logitEnabledState.js
+++ src/utilities/logitEnabledState.js
@@ -1,15 +1,16 @@
 import { writable, derived, get } from 'svelte/store';
 import _ from 'lodash';
-var savedEnableState = JSON.parse(localStorage.getItem('stedsPWAdebug') || '""') || {
-  active: [],
-  inactive: [],
-};
+var savedEnableState = JSON.parse(
+  localStorage.getItem('stedsPWAdebug') || '{"active":[],"inactive":["*:*"]}',
+);
 
 console.warn('Restored logit enable state', savedEnableState);
 export const logitEnabledState = writable(savedEnableState);
 export const logitEnabledString = derived(logitEnabledState, ($logitEnabledState) => {
   const { active, inactive } = $logitEnabledState;
-  return [...active, ...inactive.map((n) => '-' + n)].join(' ');
+  let values = [...active.map((v) => [v, '']), ...inactive.map((v) => [v, '-'])];
+  values = _.sortBy(values, (v) => v[0]);
+  return values.map(([v, s]) => s + v).join(' ');
 });
 export const logitEnabledStringTrace = derived(
   logitEnabledString,

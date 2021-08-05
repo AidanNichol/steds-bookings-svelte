8c40b5100d8fef00364a9c34552c9874a520f512
Aidan Nichol <aidan@nicholware.co.uk>
2021-08-05 10:35:54 +0100

    add reload of accounts/members events

--- src/store/monitorChanges.js
+++ src/store/monitorChanges.js
@@ -1,10 +1,14 @@
 // import { writable } from 'svelte/store';
 
 // import { fetchData } from '@utils/use-data-api';
+import { now } from '@utils/dateFns';
 import { refreshAccountBookings } from './accountStatus.js';
 import { refreshedBookingStatus } from './walkBookingStatus';
 import { updateMemberIndex } from './membersList';
 import { refreshMemberData } from './memberCurrent.js';
+import { loadMembers } from '@store/membersList';
+import { loadAccounts } from '@store/accountsList';
+import { setStale } from './payments.js';
 import Logit from '@utils/logit';
 
 var logit = Logit('store/monitorChanges');
@@ -16,7 +20,7 @@ const createEvents = () => {
   const host = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:4444';
   let events = new EventSource(`${host}/bookingsServer/monitorChanges?who=app${retry++}`);
   events.onerror = (error) => {
-    logit('error', error);
+    logit('Monitor Changes error', now(), error);
   };
   events.onmessage = (e) => {
     let { lastEventId: id, data, type } = e;
@@ -36,7 +40,10 @@ const createEvents = () => {
   events.addEventListener('bookingChange', genListener(refreshAccountBookings));
   events.addEventListener('memberChange', genListener(refreshMemberData));
   events.addEventListener('refreshMemberIndex', genListener(updateMemberIndex));
+  events.addEventListener('refreshMemberList', genListener(loadMembers));
+  events.addEventListener('refreshAccountList', genListener(loadAccounts));
   events.addEventListener('refreshBookingCount', genListener(refreshedBookingStatus));
+  events.addEventListener('refreshBanking', genListener(setStale));
   return events;
 };
 createEvents();

a7684f290c93687bc60099b9fc9e05922b46e48c
Aidan Nichol <aidan@nicholware.co.uk>
2022-02-14 11:01:59 +0000

    remove commented lines

--- src/store/store.js
+++ src/store/store.js
@@ -1,29 +1,15 @@
-// import { createImmerActionStore } from '@utils/createImmerActionStore';
-// import { fetchData } from '@utils/use-data-api';
 import { writable, get } from 'svelte/store';
 
-// import { loadStatus as loadBookingStatus } from '@store/walkBookingStatus';
-// import { patches } from './patches';
 import { loadMembers } from '@store/membersList';
 import { loadWalks } from '@store/walksList';
 import { loadAccounts } from '@store/accountsList';
-import { currentMemberId } from '@store/memberCurrent';
+// import { currentMemberId } from '@store/memberCurrent';
 
-// import { banking } from './banking';
-// import { socket } from './socket';
-// import { payments } from './payments';
-// import { reports } from './reports';
 import { getStartDate } from './accountStatus';
-// import { debugSettings } from './debugSettings';
-// import { router } from './router';
-// // import { SW } from './SW';
-// import { user } from './user';
 import Logit from '@utils/logit';
-// import { getWeekYearWithOptions } from 'date-fns/fp';
 
 var logit = Logit('store/index');
 
-// note the use of this which refers to observable instance of the store
 export const loaded = writable(false);
 export const loading = writable(false);
 export const firstBookingDate = writable(null);
@@ -46,8 +32,6 @@ export const hydrate = async () => {
   await loadNames();
   const fBooking = await getStartDate();
   firstBookingDate.set(fBooking);
-  // await loadBookingStatus(true);
-  // await banking.getLatestBanking();
   loading.set(false);
   loaded.set(true);
   setTimeout(() => {
@@ -56,6 +40,4 @@ export const hydrate = async () => {
     // currentMemberId.set('M853');
     // currentMemberId.set('M2009');
   }, 100);
-  // await socket.load();
-  // await debugSettings.load();
 };

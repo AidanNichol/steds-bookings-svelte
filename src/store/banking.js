450b9275f4cdd40138b903c609fc54e697b44322
Aidan Nichol <aidan@nicholware.co.uk>
2023-03-30 13:49:37 +0100

    fix calculation of totalCredits

--- src/store/banking.js
+++ src/store/banking.js
@@ -1,12 +1,12 @@
-import { fetchData } from '@utils/use-data-api';
-import { getTimestamp } from '@utils/dateFns';
-import { writable, get } from 'svelte/store';
-import { clearPaymentsMade } from './payments';
+import { fetchData } from "@utils/use-data-api";
+import { getTimestamp } from "@utils/dateFns";
+import { writable, get } from "svelte/store";
+import { clearPaymentsMade } from "./payments";
 
-import { addToQueue } from './patches.js';
+import { addToQueue } from "./patches.js";
 
-import Logit from '@utils/logit';
-var logit = Logit('store/banking');
+import Logit from "@utils/logit";
+const logit = Logit("store/banking");
 /*
     ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
     ┃                    useLatestBanking                      ┃
@@ -15,39 +15,39 @@ var logit = Logit('store/banking');
 export const showBanking = writable(false);
 export const latestBanking = writable(null);
 export const getLatestBanking = async () => {
-  const res = await fetchData(`banking/latest`);
-  logit('fetchData returned', res);
-  latestBanking.set(res);
-  logit('latestBanking', get(latestBanking));
+	const res = await fetchData("banking/latest");
+	logit("fetchData returned", res);
+	latestBanking.set(res);
+	logit("latestBanking", get(latestBanking));
 };
 export const bankMoney = async (paid, paidTotal, totalDebt, totalCredit) => {
-  const latest = get(latestBanking);
-  const timeS = getTimestamp();
-  const newBanking = {
-    bankingId: 'BP' + timeS,
-    bankedAmount: paidTotal,
-    closingDebt: totalDebt,
-    closingCredit: totalCredit,
-    openingCredit: latest.closingCredit,
-    openingDebt: latest.closingDebt,
-    endDate: timeS,
-    startDate: latest.endDate,
-  };
-  const patch = { op: 'add', path: ['Banking'], value: newBanking };
-  const patches = [patch];
-  paid.forEach((a) => {
-    a.payments.forEach((p) =>
-      patches.push({
-        op: 'update',
-        path: ['Payment', p.paymentId, 'bankingId'],
-        value: 'BP' + timeS,
-      }),
-    );
-  });
+	const latest = get(latestBanking);
+	const timeS = getTimestamp();
+	const newBanking = {
+		bankingId: `BP${timeS}`,
+		bankedAmount: paidTotal,
+		closingDebt: totalDebt,
+		closingCredit: totalCredit ?? 0,
+		openingCredit: latest.closingCredit ?? 0,
+		openingDebt: latest.closingDebt,
+		endDate: timeS,
+		startDate: latest.endDate,
+	};
+	const patch = { op: "add", path: ["Banking"], value: newBanking };
+	const patches = [patch];
+	paid.forEach((a) => {
+		a.payments.forEach((p) =>
+			patches.push({
+				op: "update",
+				path: ["Payment", p.paymentId, "bankingId"],
+				value: `BP${timeS}`,
+			}),
+		);
+	});
 
-  logit('bankMoney patches', patches);
-  addToQueue([patches, [{}]]);
-  latestBanking.set(newBanking);
-  clearPaymentsMade();
+	logit("bankMoney patches", patches);
+	addToQueue([patches, [{}]]);
+	latestBanking.set(newBanking);
+	clearPaymentsMade();
 };
 getLatestBanking();
--- src/store/payments.js
+++ src/store/payments.js
@@ -1,13 +1,13 @@
-import { writable, derived } from 'svelte/store';
-import { fetchData } from '@utils/use-data-api';
-import { dispDate } from '@utils/dateFns';
-import { nameIndex as names } from '@store/nameIndex.js';
-import { loaded } from '@store/store';
+import { writable, derived } from "svelte/store";
+import { fetchData } from "@utils/use-data-api";
+import { dispDate } from "@utils/dateFns";
+import { nameIndex as names } from "@store/nameIndex.js";
+import { loaded } from "@store/store";
 // import { page } from '@store/router.js';
 
-import _ from 'lodash';
-import Logit from '@utils/logit';
-var logit = Logit('store/payments');
+import _ from "lodash";
+import Logit from "@utils/logit";
+const logit = Logit("store/payments");
 const staleCredits = writable(true);
 const staleDebts = writable(true);
 const stalePayments = writable(true);
@@ -29,9 +29,9 @@ export const displayDebts = writable(true);
 // };
 
 export const setStale = () => {
-  staleCredits.set(true);
-  staleDebts.set(true);
-  stalePayments.set(true);
+	staleCredits.set(true);
+	staleDebts.set(true);
+	stalePayments.set(true);
 };
 // payments.onChanges = thunkOn(
 //   (actions, storeActions) => [
@@ -54,38 +54,38 @@ export const setStale = () => {
 
 let debtsDue = [];
 export const debts = derived(
-  [staleDebts, names, loaded],
-  async ([$staleDebts, $names, $loaded], set) => {
-    if (!$staleDebts || !$loaded) {
-      set(debtsDue);
-      return;
-    }
+	[staleDebts, names, loaded],
+	async ([$staleDebts, $names, $loaded], set) => {
+		if (!$staleDebts || !$loaded) {
+			set(debtsDue);
+			return;
+		}
 
-    const debtsData = await fetchData(`booking/owing`);
-    logit('fetchData  Booking/owing returned', debtsData);
+		const debtsData = await fetchData("booking/owing");
+		logit("fetchData  Booking/owing returned", debtsData);
 
-    let dMap = debtsData.map((b) => {
-      const { accountId, shortName: name } = $names?.get(b.memberId) ?? {};
-      const { sortName } = $names?.get(accountId) ?? {};
-      // logit('test b', b, $names?.get(b.memberId), accountId, $names?.get(accountId));
-      const { venue } = $names?.get(b.walkId) ?? {};
-      const displayDate = dispDate(b.BookingLogs[0].id);
-      return { ...b, name, venue, displayDate, accountId, sortName };
-    });
-    dMap = _.groupBy(dMap, (b) => b.sortName);
-    const pairs = _.sortBy(_.toPairs(dMap), (item) => item[0]);
-    debtsDue = pairs.map(([sortName, bookings]) => {
-      const balance = bookings.reduce((tot, b) => tot + b.owing, 0);
-      const accountId = bookings[0].accountId;
-      return { sortName, accountId, balance, bookings };
-    });
-    logit('debts', debtsDue);
-    set(debtsDue);
-    staleDebts.set(false);
-  },
+		let dMap = debtsData.map((b) => {
+			const { accountId, shortName: name } = $names?.get(b.memberId) ?? {};
+			const { sortName } = $names?.get(accountId) ?? {};
+			// logit('test b', b, $names?.get(b.memberId), accountId, $names?.get(accountId));
+			const { venue } = $names?.get(b.walkId) ?? {};
+			const displayDate = dispDate(b.BookingLogs[0].id);
+			return { ...b, name, venue, displayDate, accountId, sortName };
+		});
+		dMap = _.groupBy(dMap, (b) => b.sortName);
+		const pairs = _.sortBy(_.toPairs(dMap), (item) => item[0]);
+		debtsDue = pairs.map(([sortName, bookings]) => {
+			const balance = bookings.reduce((tot, b) => tot + b.owing, 0);
+			const accountId = bookings[0].accountId;
+			return { sortName, accountId, balance, bookings };
+		});
+		logit("debts", debtsDue);
+		set(debtsDue);
+		staleDebts.set(false);
+	},
 );
 export const totalDebt = derived(debts, ($payload) =>
-  $payload.reduce((tot, a) => tot + a.balance, 0),
+	$payload.reduce((tot, a) => tot + a.balance, 0),
 );
 /*
     ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
@@ -95,46 +95,46 @@ export const totalDebt = derived(debts, ($payload) =>
 
 let paymentsData = [];
 export const clearPaymentsMade = () => {
-  paymentsData = [];
-  stalePayments.set(true);
+	paymentsData = [];
+	stalePayments.set(true);
 };
 export const paid = derived(
-  [stalePayments, names, loaded],
-  async ([$stalePayments, $names, $loaded], set) => {
-    if (!$stalePayments || !$loaded) return set(paymentsData);
+	[stalePayments, names, loaded],
+	async ([$stalePayments, $names, $loaded], set) => {
+		if (!$stalePayments || !$loaded) return set(paymentsData);
 
-    paymentsData = await fetchData(`payment/paymentsMade`);
-    logit('fetchData returned', paymentsData);
+		paymentsData = await fetchData("payment/paymentsMade");
+		logit("fetchData returned", paymentsData);
 
-    let dMap = paymentsData.map((p) => {
-      p.displayDate = dispDate(p.paymentId);
-      p.Bookings = p.Allocations.map((a) => {
-        const booking = a.Booking;
-        const { shortName: name } = $names.get(booking.memberId);
-        const { venue } = $names.get(booking.walkId);
-        const displayDate = dispDate(booking.BookingLogs[0].id);
-        return { ...booking, name, venue, amount: a.amount, displayDate };
-      }).filter((p) => p.fee !== 0);
-      const { sortName } = $names.get(p.accountId);
-      return { ...p, sortName };
-    });
-    dMap = _.groupBy(dMap, (p) => p.sortName);
-    const pairs = _.sortBy(_.toPairs(dMap), (item) => item[0]);
-    const paymentsMade = pairs.map(([sortName, payments]) => {
-      const balance = payments.reduce((tot, p) => tot + p.amount, 0);
-      const accountId = payments[0].accountId;
-      return { sortName, accountId, balance, payments };
-    });
-    logit('paymentsMade', paymentsMade);
-    paymentsData = paymentsMade;
-    set(paymentsMade);
-    stalePayments.set(false);
-  },
-  [],
+		let dMap = paymentsData.map((p) => {
+			p.displayDate = dispDate(p.paymentId);
+			p.Bookings = p.Allocations.map((a) => {
+				const booking = a.Booking;
+				const { shortName: name } = $names.get(booking.memberId);
+				const { venue } = $names.get(booking.walkId);
+				const displayDate = dispDate(booking.BookingLogs[0].id);
+				return { ...booking, name, venue, amount: a.amount, displayDate };
+			}).filter((p) => p.fee !== 0);
+			const { sortName } = $names.get(p.accountId);
+			return { ...p, sortName };
+		});
+		dMap = _.groupBy(dMap, (p) => p.sortName);
+		const pairs = _.sortBy(_.toPairs(dMap), (item) => item[0]);
+		const paymentsMade = pairs.map(([sortName, payments]) => {
+			const balance = payments.reduce((tot, p) => tot + p.amount, 0);
+			const accountId = payments[0].accountId;
+			return { sortName, accountId, balance, payments };
+		});
+		logit("paymentsMade", paymentsMade);
+		paymentsData = paymentsMade;
+		set(paymentsMade);
+		stalePayments.set(false);
+	},
+	[],
 );
 
 export const totalPaid = derived(paid, ($payload) =>
-  $payload.reduce((tot, a) => tot + a.balance, 0),
+	$payload.reduce((tot, a) => tot + a.balance, 0),
 );
 /*
     ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
@@ -143,19 +143,19 @@ export const totalPaid = derived(paid, ($payload) =>
 */
 let creditsData = [];
 export const credits = derived(
-  staleCredits,
-  async ($staleCredits, set) => {
-    if (!$staleCredits) return set(creditsData);
-    creditsData = await fetchData(`account/creditsOwed`);
-    logit('fetchData returned', creditsData);
-    set(creditsData);
-    $staleCredits = false;
-  },
-  [],
+	staleCredits,
+	async ($staleCredits, set) => {
+		if (!$staleCredits) return set(creditsData);
+		creditsData = await fetchData("account/creditsOwed");
+		logit("fetchData returned", creditsData);
+		set(creditsData);
+		$staleCredits = false;
+	},
+	[],
 );
 
 export const totalCredit = derived(
-  credits,
-  ($credits) => $credits.reduce((tot, a) => tot + a.balance, 0),
-  0,
+	credits,
+	($credits) => $credits.reduce((tot, a) => tot + a.available ?? 0, 0),
+	0,
 );
